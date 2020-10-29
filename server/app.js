3f90821b1c51c83b9d46c303555aae4b6fbdf65f
Bereket Girma <bgirma@Berekets-MacBook-Pro.local>
2020-10-29 16:12:37 -0700

    add BE routes

--- server/app.js
+++ server/app.js
@@ -1,16 +1,35 @@
 require('dotenv').config()
-console.log(process.env.USERNAME || process)
-
 const express = require("express");
-const jwtMiddleware = require('express-jwt')
-const jwtToken = require('jsonwebtoken')
 const app = express();
 const PORT = 3001;
+
 const exampleRoutes = require("./routes/example");
+const authRoutes = require('./routes/auth')
+
+// const jwtMiddleware = require('express-jwt')
+// const jwtToken = require('jsonwebtoken')
+
+const venuesRoutes = require('./routes/venues')
+const venue_labelsRoutes = require('./routes/venue_labels')
+const locationRoutes = require('./routes/locations')
+const labelsRoutes = require('./routes/labels')
+const happy_hrRoutes = require('./routes/happy_hr')
+const galleriesRoutes = require('./routes/galleries')
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-const authRoutes = require('./routes/auth')
+
+
 app.use("/api", exampleRoutes);
+app.use("/api/", venuesRoutes);
+app.use("/api/", venue_labelsRoutes);
+app.use("/api/", locationRoutes);
+app.use("/api/", labelsRoutes);
+app.use("/api/",happy_hrRoutes );
+app.use("/api/", galleriesRoutes);
+
+
 app.listen(PORT, () => {
   console.log("running on port 3001");
 });
