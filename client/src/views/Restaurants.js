a70a2510db692326dee521be7438e186fadcc115
Will Stoddard <wstoddard89@gmail.com>
2020-10-29 15:58:29 -0700

    build initial parallax scroll

--- client/src/views/Restaurants.js
+++ client/src/views/Restaurants.js
@@ -1,5 +1,18 @@
 import React from "react"
+import styles from './Restaurants.module.css';
 
 export default function Restaurants() {
-  return <h1>Restaturants Page</h1>
+  return (
+    <div className={styles.container}>
+      <div className={styles.box1}>
+        <h1>Parallax Effect 1</h1>
+      </div>
+      <div className={styles.box2}>
+        <h1>Parallax Effect 2</h1>
+      </div>
+      <div className={styles.box3}>
+        <h1>Parallax Effect 3</h1>
+      </div>
+    </div>
+  )
 }
