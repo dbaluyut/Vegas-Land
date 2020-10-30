e3cd1d15012c0976af5e50374686dc2762320405
Will Stoddard <wstoddard89@gmail.com>
2020-10-30 09:10:49 -0700

    more parallax scroll

--- client/src/views/Restaurants.js
+++ client/src/views/Restaurants.js
@@ -3,9 +3,12 @@ import styles from './Restaurants.module.css';
 
 export default function Restaurants() {
   return (
+    <div className={styles.backgroundContainer}>
     <div className={styles.container}>
+      <div className={styles.backgroundContainer2}>
       <div className={styles.box1}>
         <h1>Parallax Effect 1</h1>
+      </div>
       </div>
       <div className={styles.box2}>
         <h1>Parallax Effect 2</h1>
@@ -13,6 +16,7 @@ export default function Restaurants() {
       <div className={styles.box3}>
         <h1>Parallax Effect 3</h1>
       </div>
+    </div>
     </div>
   )
 }
