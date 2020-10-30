1cd44f18a7a23617e55fe5f0a800cf257e5f115b
Will Stoddard <wstoddard89@gmail.com>
2020-10-30 14:25:33 -0700

    finish parallax

--- client/src/views/Restaurants.js
+++ client/src/views/Restaurants.js
@@ -1,22 +1,22 @@
 import React from "react"
-import styles from './Restaurants.module.css';
+import styles from "./Restaurants.module.css"
 
 export default function Restaurants() {
   return (
-    <div className={styles.backgroundContainer}>
-    <div className={styles.container}>
-      <div className={styles.backgroundContainer2}>
-      <div className={styles.box1}>
-        <h1>Parallax Effect 1</h1>
+    <div className={styles.fullContainer}>
+      <div className={styles.header}></div>
+
+      <div className={styles.container}>
+        <div className={styles.box1}>
+          <h1>Restaurant Title</h1>
+        </div>
+        <div className={styles.box2}>
+          <h1>Restaurant Description</h1>
+        </div>
+        <div className={styles.box3}>
+          <h1>Parallax Effect 3</h1>
+        </div>
       </div>
-      </div>
-      <div className={styles.box2}>
-        <h1>Parallax Effect 2</h1>
-      </div>
-      <div className={styles.box3}>
-        <h1>Parallax Effect 3</h1>
-      </div>
-    </div>
     </div>
   )
 }
