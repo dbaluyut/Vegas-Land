f435c2ee6fbe84191cf01794f7c1ec1179ba5dfb
Will Stoddard <wstoddard89@gmail.com>
2020-10-31 11:50:58 -0700

    made slice and mapped

--- client/src/views/Experiences.js
+++ client/src/views/Experiences.js
@@ -1,196 +1,34 @@
-import React from "react"
+import React, { useEffect } from "react"
 import styles from "./Experiences.module.css"
-import { Navbar } from "../ui/Navbar";
+import { Navbar } from "../ui/Navbar"
+import { selectExperiences, getExperiences } from "./experiencesSlice"
+import { useSelector, useDispatch } from "react-redux"
 
 export default function Experiences() {
-  return (
-  <div>
-    <div className={styles.header}>
-    <Navbar />
-    </div>
-    <div className={styles.container}>
-  <div className={styles.card}>
-    <div className={styles.content}>
-      <div className={styles.center}>
-        <h3>
-        Example Content 1
-        </h3>
-      </div>
-    </div>
-  </div>
-  <div className={styles.card}>
-    <div className={styles.content}>
-      <div className={styles.center}>
-        <h3>
-        Example Content 2
-        </h3>
-      </div>
-    </div>
-  </div>
-  <div className={styles.card}>
-    <div className={styles.content}>
-      <div className={styles.center}>
-        <h3>
-        Example Content 3
-        </h3>
-      </div>
-    </div>
-  </div>
-  <div className={styles.card}>
-    <div className={styles.content}>
-      <div className={styles.center}>
-        <h3>
-        Example Content 4
-        </h3>
-      </div>
-    </div>
-  </div>
-  <div className={styles.card}>
-    <div className={styles.content}>
-      <div className={styles.center}>
-        <h3>
-        Example Content 5
-        </h3>
-      </div>
-    </div>
-  </div>
-  <div className={styles.card}>
-    <div className={styles.content}>
-      <div className={styles.center}>
-        <h3>
-        Example Content 6
-        </h3>
-      </div>
-    </div>
-  </div>
-  <div className={styles.card}>
-    <div className={styles.content}>
-      <div className={styles.center}>
-        <h3>
-        Example Content 7
-        </h3>
-      </div>
-    </div>
-  </div>
-  <div className={styles.card}>
-    <div className={styles.content}>
-      <div className={styles.center}>
-      <h3>
-        Example Content 8
-        </h3>
-      </div>
-    </div>
-  </div>
-  <div className={styles.card}>
-    <div className={styles.content}>
-      <div className={styles.center}>
-      <h3>
-        Example Content 9
-        </h3>
-      </div>
-    </div>
-  </div>
-  <div className={styles.card}>
-    <div className={styles.content}>
-      <div className={styles.center}>
-      <h3>
-        Example Content 10
-        </h3>
-      </div>
-    </div>
-  </div>
-  <div className={styles.card}>
-    <div className={styles.content}>
-      <div className={styles.center}>
-      <h3>
-        Example Content 11
-        </h3>
-      </div>
-    </div>
-  </div>
-  <div className={styles.card}>
-    <div className={styles.content}>
-      <div className={styles.center}>
-      <h3>
-        Example Content 12
-        </h3>
-      </div>
-    </div>
-  </div>
-  <div className={styles.card}>
-    <div className={styles.content}>
-      <div className={styles.center}>
-      <h3>
-        Example Content 13
-        </h3>
-      </div>
-    </div>
-  </div>
-  <div className={styles.card}>
-    <div className={styles.content}>
-      <div className={styles.center}>
-      <h3>
-        Example Content 14
-        </h3>
-      </div>
-    </div>
-  </div>
-  <div className={styles.card}>
-    <div className={styles.content}>
-      <div className={styles.center}>
-      <h3>
-        Example Content 15
-        </h3>
-      </div>
-    </div>
-  </div>
-  <div className={styles.card}>
-    <div className={styles.content}>
-      <div className={styles.center}>
-      <h3>
-        Example Content 16
-        </h3>
-      </div>
-    </div>
-  </div>
-  <div className={styles.card}>
-    <div className={styles.content}>
-      <div className={styles.center}>
-      <h3>
-        Example Content 17
-        </h3>
-      </div>
-    </div>
-  </div>
-  <div className={styles.card}>
-    <div className={styles.content}>
-      <div className={styles.center}>
-      <h3>
-        Example Content 18
-        </h3>
-      </div>
-    </div>
-  </div>
-  <div className={styles.card}>
-    <div className={styles.content}>
-      <div className={styles.center}>
-      <h3>
-        Example Content 19
-        </h3>
-      </div>
-    </div>
-  </div>
-  <div className={styles.card}>
-    <div className={styles.content}>
-      <div className={styles.center}>
-      <h3>
-        Example Content 20
-        </h3>
-      </div>
-    </div>
-  </div>
-</div>
-</div>
-   );
-}
+  const experiences = useSelector(selectExperiences)
+  const dispatch = useDispatch()
 
+  useEffect(() => {
+    dispatch(getExperiences())
+  }, [])
+  console.log(experiences)
+  return (
+    <div>
+      <div className={styles.header}>
+        <Navbar />
+      </div>
+
+      <div className={styles.container}>
+        {experiences.map((item) => (
+          <div className={styles.card}>
+            <div className={styles.content}>
+              <div className={styles.center}>
+                <h3>{item.title}</h3>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
--- client/src/views/Restaurants.js
+++ client/src/views/Restaurants.js
@@ -5,8 +5,9 @@ import styles from "./Restaurants.module.css"
 export default function Restaurants() {
   return (
     <div className={styles.fullContainer}>
+      <div className={styles.header}>
       <Navbar />
-
+      </div>
       <main className={styles.wrapper}>
       <section className={`${styles.section} ${styles.parallax} ${styles.bg1}`}>
         <h1>Restaurant 1</h1>
