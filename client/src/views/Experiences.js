2d09506fe65d69a3209544999f2fc2f07707e89c
Will Stoddard <wstoddard89@gmail.com>
2020-10-31 10:11:10 -0700

    start layout for experiences. rework parallax

--- client/src/views/Experiences.js
+++ client/src/views/Experiences.js
@@ -1,13 +1,196 @@
 import React from "react"
-
+import styles from "./Experiences.module.css"
 import { Navbar } from "../ui/Navbar";
 
 export default function Experiences() {
   return (
   <div>
+    <div className={styles.header}>
     <Navbar />
-   <h1>Experiences page</h1> 
-   </div>
+    </div>
+    <div className={styles.container}>
+  <div className={styles.card}>
+    <div className={styles.content}>
+      <div className={styles.center}>
+        <h3>
+        Example Content 1
+        </h3>
+      </div>
+    </div>
+  </div>
+  <div className={styles.card}>
+    <div className={styles.content}>
+      <div className={styles.center}>
+        <h3>
+        Example Content 2
+        </h3>
+      </div>
+    </div>
+  </div>
+  <div className={styles.card}>
+    <div className={styles.content}>
+      <div className={styles.center}>
+        <h3>
+        Example Content 3
+        </h3>
+      </div>
+    </div>
+  </div>
+  <div className={styles.card}>
+    <div className={styles.content}>
+      <div className={styles.center}>
+        <h3>
+        Example Content 4
+        </h3>
+      </div>
+    </div>
+  </div>
+  <div className={styles.card}>
+    <div className={styles.content}>
+      <div className={styles.center}>
+        <h3>
+        Example Content 5
+        </h3>
+      </div>
+    </div>
+  </div>
+  <div className={styles.card}>
+    <div className={styles.content}>
+      <div className={styles.center}>
+        <h3>
+        Example Content 6
+        </h3>
+      </div>
+    </div>
+  </div>
+  <div className={styles.card}>
+    <div className={styles.content}>
+      <div className={styles.center}>
+        <h3>
+        Example Content 7
+        </h3>
+      </div>
+    </div>
+  </div>
+  <div className={styles.card}>
+    <div className={styles.content}>
+      <div className={styles.center}>
+      <h3>
+        Example Content 8
+        </h3>
+      </div>
+    </div>
+  </div>
+  <div className={styles.card}>
+    <div className={styles.content}>
+      <div className={styles.center}>
+      <h3>
+        Example Content 9
+        </h3>
+      </div>
+    </div>
+  </div>
+  <div className={styles.card}>
+    <div className={styles.content}>
+      <div className={styles.center}>
+      <h3>
+        Example Content 10
+        </h3>
+      </div>
+    </div>
+  </div>
+  <div className={styles.card}>
+    <div className={styles.content}>
+      <div className={styles.center}>
+      <h3>
+        Example Content 11
+        </h3>
+      </div>
+    </div>
+  </div>
+  <div className={styles.card}>
+    <div className={styles.content}>
+      <div className={styles.center}>
+      <h3>
+        Example Content 12
+        </h3>
+      </div>
+    </div>
+  </div>
+  <div className={styles.card}>
+    <div className={styles.content}>
+      <div className={styles.center}>
+      <h3>
+        Example Content 13
+        </h3>
+      </div>
+    </div>
+  </div>
+  <div className={styles.card}>
+    <div className={styles.content}>
+      <div className={styles.center}>
+      <h3>
+        Example Content 14
+        </h3>
+      </div>
+    </div>
+  </div>
+  <div className={styles.card}>
+    <div className={styles.content}>
+      <div className={styles.center}>
+      <h3>
+        Example Content 15
+        </h3>
+      </div>
+    </div>
+  </div>
+  <div className={styles.card}>
+    <div className={styles.content}>
+      <div className={styles.center}>
+      <h3>
+        Example Content 16
+        </h3>
+      </div>
+    </div>
+  </div>
+  <div className={styles.card}>
+    <div className={styles.content}>
+      <div className={styles.center}>
+      <h3>
+        Example Content 17
+        </h3>
+      </div>
+    </div>
+  </div>
+  <div className={styles.card}>
+    <div className={styles.content}>
+      <div className={styles.center}>
+      <h3>
+        Example Content 18
+        </h3>
+      </div>
+    </div>
+  </div>
+  <div className={styles.card}>
+    <div className={styles.content}>
+      <div className={styles.center}>
+      <h3>
+        Example Content 19
+        </h3>
+      </div>
+    </div>
+  </div>
+  <div className={styles.card}>
+    <div className={styles.content}>
+      <div className={styles.center}>
+      <h3>
+        Example Content 20
+        </h3>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
    );
 }
 
