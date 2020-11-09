4615e96a2f28300eae0534fa5e4da7dd4f92aae6
Bereket Girma <bgirma@Berekets-MacBook-Pro.local>
2020-11-09 12:03:13 -0800

    work on update page

--- client/src/views/admin/Update.js
+++ client/src/views/admin/Update.js
@@ -1,44 +1,136 @@
-import React, { useState, useEffect } from "react"
-import { useSelector, useDispatch } from "react-redux"
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 // import { DashboardTable } from "../../ui/DashboardTable"
-// import styles from "./Dashboard.module.css"
+import styles from "./Update.module.css";
 
 import {
   display,
   getVenue,
-  addVenue,
-  selectDashboard,
-} from "./dashboardSlice.js"
+  // addVenue,
+  updateVenue,
+  selectUpdate,
+} from "./updateSlice.js";
 
 export default function Update() {
-  const dispatch = useDispatch()
-  const venues = useSelector(selectDashboard)
-  console.log(venues)
-
-  const [title, setTitle] = useState("")
-  const [desc, setDesc] = useState("")
-  const [location_id, setLocation_id] = useState("")
-  const [type, setType] = useState("")
-  const [link, setLink] = useState("")
+  const dispatch = useDispatch();
+  const venues = useSelector(selectUpdate);
+  const [venueMap, setVenueMap] = useState({});
 
   useEffect(() => {
-    dispatch(getVenue())
-  }, [])
+    const vMap = {};
+    venues.map((item) => {
+      vMap[item.id] = item;
+    });
+    setVenueMap(vMap);
+  }, [venues]);
 
-  function handleSubmit(e) {
-    e.preventDefault()
-    dispatch(addVenue({ title, desc, location_id, type, link }))
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
+  const [location_id, setLocation_id] = useState("");
+  const [type, setType] = useState("");
+  const [link, setLink] = useState("");
 
-    setTitle("")
-    setDesc("")
-    setLocation_id("")
-    setType("")
-    setLink("")
+  const [activeVenue, setActiveVenue] = useState("");
+
+  useEffect(() => {
+    dispatch(getVenue());
+  }, []);
+
+  // function handleSubmit(e) {
+  //   e.preventDefault()
+  //   dispatch(addVenue({ title, desc, location_id, type, link }))
+
+  //   setTitle("")
+  //   setDesc("")
+  //   setLocation_id("")
+  //   setType("")
+  //   setLink("")
+  // }
+
+  const handleSelect = (e) => {
+    /// select venue from map
+    const id = e.target.value;
+    console.log(venueMap[id]);
+    setActiveVenue(e.target.value);
+  };
+
+  function handleUpdateVenue(venue) {
+    dispatch(updateVenue({ title, desc, type, link }));
   }
 
   // function handleUpdate(name, desc) {
   //   dispatch(updateRecommendations({ name, desc }));
   // }
 
-  return <div>test</div>
+  return (
+    <div className={styles.dashContainer}>
+      <div className={styles.dashboardSidebar}>
+        <h1>update page view</h1>
+        <form className={styles.form} onSubmit={handleUpdateVenue}>
+          {/* dropdown */}
+          <div
+            className={styles.dropdown}
+            // onChange={setActiveVenue}
+            // onChange={(e) => setActiveVenue(e.target.value)}
+          >
+            <label for="venues">Venues</label>
+            <select
+              className={styles.type}
+              value={activeVenue}
+              onChange={handleSelect}
+            >
+              <optgroup label="Venues:">
+                {venues.map((venue) => {
+                  return <option value={venue.id}>{venue.title}</option>;
+                })}
+              </optgroup>
+            </select>
+          </div>
+
+          {/* title form */}
+          <div className={styles.title}>
+            <label for="title">Title</label>
+            <input type="text" value={activeVenue.title} />
+          </div>
+          {/* description form */}
+          <div className={styles.desc}>
+            <label for="desc">Desc</label>
+            <textarea></textarea>
+          </div>
+          {/* Location_id form */}
+          <div className={styles.location_id}>
+            <label for="location_id">Location_id</label>
+            <input type="number" />
+          </div>
+
+          {/* Type form */}
+          <div className={styles.type}>
+            <label for="type">Venue Type</label>
+            <select required="" className={styles.type}>
+              <option disabled="" value="" selected="selected">
+                Select Type
+              </option>
+              <optgroup label="Venue type:">
+                <option value="bar">bar</option>
+                <option value="experience">Experience</option>
+                <option value="restaurant">Restaurant</option>
+                <option value="shop">Shop</option>
+              </optgroup>
+            </select>
+          </div>
+
+          {/* Link form */}
+          <div className={styles.link}>
+            <label for="link">Link</label>
+            <input type="text" value={link} />
+          </div>
+          <div className={styles.submit}>
+            <button className={styles.submit} type="submit" value="Submit">
+              Update
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
--- client/src/views/admin/updateSlice.js
+++ client/src/views/admin/updateSlice.js
@@ -1,41 +1,53 @@
-import { createSlice } from "@reduxjs/toolkit"
-import axios from "axios"
+import { createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 
-export const dashboardSlice = createSlice({
-  name: "dashboard",
+export const updateSlice = createSlice({
+  name: "update",
   initialState: {
-    dashboard: [],
+    update: [],
   },
 
   reducers: {
     display: (state, action) => {
-      state.dashboard = action.payload
+      state.update = action.payload;
     },
   },
-})
+});
 
-const { display } = dashboardSlice.actions
+const { display } = updateSlice.actions;
 
 export const getVenue = () => (dispatch) => {
-  axios.get("api/venues").then((r) => dispatch(display(r.data)))
-}
+  axios.get("api/venues").then((r) => dispatch(display(r.data)));
+};
 
-export const addVenue = (venue) => (dispatch) => {
-  console.log(venue)
+// export const addVenue = (venue) => (dispatch) => {
+//   console.log(venue)
+//   axios
+//     .post("api/venues", {
+//       title: venue.title,
+//       desc: venue.desc,
+//       location_id: venue.location_id,
+//       type: venue.type,
+//       link: venue.link,
+//     })
+//     .then((r) => {
+//       dispatch(getVenue())
+//     })
+// }
+
+export const updateVenue = (venue) => (dispatch) => {
   axios
-    .post("api/venues", {
+    .patch("/api/venues/:id", {
       title: venue.title,
       desc: venue.desc,
       location_id: venue.location_id,
       type: venue.type,
       link: venue.link,
     })
-    .then((r) => {
-      dispatch(getVenue())
-    })
-}
+    .then((r) => dispatch(getVenue()));
+};
 
-export const selectDashboard = (state) => state.form.dashboard
+export const selectUpdate = (state) => state.update.update;
 // export const selectNewRecommendations = (state) => state.form.recommendations;
 
-export default dashboardSlice.reducer
+export default updateSlice.reducer;
