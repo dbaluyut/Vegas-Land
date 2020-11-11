6acf0a441755dd6f9e8ed7998d2fb24fae12a418
Bereket Girma <bgirma@Berekets-MacBook-Pro.local>
2020-11-11 11:30:13 -0800

    minor fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,37 @@
-import React from "react"
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import "./App.css"
+import "./App.css";
 
-import Login from "./views/admin/Login"
-import AuthRoute from "./features/authentication/AuthRoute"
-import Logout from "./views/admin/Logout"
+import Login from "./views/admin/Login";
+import AuthRoute from "./features/authentication/AuthRoute";
+import Logout from "./views/admin/Logout";
 
-import WelcomeScreen from "./views/WelcomeScreen"
-import About from "./views/About"
-import Home from "./views/Home"
-import Bars from "./views/Bars"
-import Restaurants from "./views/Restaurants"
-import Experiences from "./views/Experiences"
-import Dashboard from "./views/admin/Dashboard"
-import Update from "./views/admin/Update"
-import Recommendations from "./views/Recommendations"
-import RecommendationsTable from "./views/admin/RecommendationsTable"
-import Feedback from "./views/Feedback"
-import HappyHour from "./views/HappyHour"
-import AddVenue from "./views/admin/AddVenue"
+import WelcomeScreen from "./views/WelcomeScreen";
+import About from "./views/About";
+import Home from "./views/Home";
+import Bars from "./views/Bars";
+import Restaurants from "./views/Restaurants";
+import Experiences from "./views/Experiences";
+import Dashboard from "./views/admin/Dashboard";
+import Update from "./views/admin/Update";
+import Recommendations from "./views/Recommendations";
+import RecommendationsTable from "./views/admin/RecommendationsTable";
+import Feedback from "./views/Feedback";
+import HappyHour from "./views/HappyHour";
+import AddVenue from "./views/admin/AddVenue";
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route exact path="/">
-          <WelcomeScreen />
+          <Home />
         </Route>
         <Route path="/about">
           <About />
         </Route>
-        <Route path="/home">
-          <Home />
-        </Route>
+
         <Route path="/bars/:id?">
           <Bars />
         </Route>
@@ -75,7 +73,7 @@ function App() {
         </Route>
       </Switch>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
