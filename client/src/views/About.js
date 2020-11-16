e6f8c09041133d7461c79d985130703cb78d3ef0
Bereket Girma <bgirma@Berekets-MacBook-Pro.local>
2020-11-16 10:05:50 -0800

    add bio

--- client/src/views/About.js
+++ client/src/views/About.js
@@ -15,17 +15,13 @@ export default function About() {
             <p className={styles.name}>Name</p>
             <p className={styles.title}>Web Developer</p>
             <p className={styles.bio}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
+            Bereket Girma is a Web Developer based in Las Vegas. She's built web applications using Node.js and React and has a solid foundation in web design and development. She recently completed PunchCode’s web development bootcamp.
+Before moving into development,  she worked in the Health sector, where she  managed and converted voice recordings that physicians and other healthcare workers make into written reports.
+When she is not coding, she enjoys spending time with family and learning different cultures and their languages. 
+Right now  she is  looking for a suitable position with a company where outstanding performance is recognised and where she can work on a variety of projects.
+
             </p>
-            <p className={styles.bio}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
-            </p>
+            
             <section className={styles.aboutIcons}>
               <a href="#">
                 <i class="fa fa-linkedin-square" aria-hidden="true"></i>
@@ -57,12 +53,7 @@ export default function About() {
               enim ad minim veniam, quis nostrud exercitation ullamco laboris
               nisi ut aliquip ex ea commodo consequat.
             </p>
-            <p className={styles.bio}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
-            </p>
+            
             <section className={styles.aboutIcons}>
               <a href="#">
                 <i class="fa fa-linkedin-square" aria-hidden="true"></i>
